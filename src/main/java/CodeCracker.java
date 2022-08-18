0e74c234306f05de933c215508ba7f4348debc13
Sypha <datonarinze@gmail.com>
2022-08-18 15:30:20 +0100

    First commit

---
+++ src/main/java/CodeCracker.java
@@ -0,0 +1,91 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+public class CodeCracker {
+    public static void main(String[] args) {
+
+    }
+
+    public static String encoder(String s) {
+        String code = "";
+        String[] str = s.toLowerCase().split("");
+        Map<String, String> encoderMap = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        encoderMap.put("a", "£");
+        encoderMap.put("b", "*");
+        encoderMap.put("c", "%");
+        encoderMap.put("d", "&");
+        encoderMap.put("e", ">");
+        encoderMap.put("f", "<");
+        encoderMap.put("g", "!");
+        encoderMap.put("h", ")");
+        encoderMap.put("i", "\"");
+        encoderMap.put("j", "(");
+        encoderMap.put("k", "@");
+        encoderMap.put("l", "a");
+        encoderMap.put("m", "b");
+        encoderMap.put("n", "c");
+        encoderMap.put("o", "d");
+        encoderMap.put("p", "e");
+        encoderMap.put("q", "f");
+        encoderMap.put("r", "g");
+        encoderMap.put("s", "h");
+        encoderMap.put("t", "i");
+        encoderMap.put("u", "j");
+        encoderMap.put("v", "k");
+        encoderMap.put("w", "l");
+        encoderMap.put("x", "m");
+        encoderMap.put("y", "n");
+        encoderMap.put("z", "o");
+
+        boolean validEntry = false;
+        int entry = 0;
+
+        while (!validEntry) {
+            System.out.println("for encryption enter 1 \n for decryption enter 2\n");
+            String input = scanner.nextLine();
+            try {
+                entry = Integer.parseInt(input);
+            } catch (Exception e) {
+                System.out.println("invalid entry " + e.getMessage());
+            }
+
+            if (entry == 1 || entry == 2) {
+                validEntry = true;
+            }
+        }
+        if (entry == 1) {
+            //starting encryption process
+            for (int i = 0; i < str.length; i++) {
+                if (encoderMap.containsKey(str[i])) {
+                    code = code + encoderMap.get(str[i]);
+                } else {
+                    code = code + str[i];
+                }
+            }
+        }
+
+        if (entry == 2) {
+
+            //starting decryption process
+            for (int i = 0; i < str.length; i++) {
+                if (encoderMap.containsKey(str[i])) {
+
+                    for (Entry<String, String> val : encoderMap.entrySet()) {
+                        if (val.getValue().equals(str[i])) {
+                            code = code + val.getKey();
+                            break;
+                        }
+
+                    }
+
+                } else {
+                    code = code + str[i];
+                }
+            }
+        }
+
+        return code;
+    }
+
+}
